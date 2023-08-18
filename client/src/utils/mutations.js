88f849962b9bc97281f09d7964d2fdaf08cb714f
Lizzz <alejandroraidi@gmail.com>
2023-08-18 17:16:49 -0600

    public refactoring

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,61 +1,75 @@
-import { gql } from "@apollo/client";
-
+import { gql } from '@apollo/client';
+// This code is so the front-ed can use the mutations defined on the back-end.
 export const LOGIN_USER = gql`
-    mutation login($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-        token
-        user {
+    mutation loginUser(
+        $email: String!
+        $password: String!
+    ) {
+        loginUser(
+            email: $email
+            password: $password
+        ) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser(
+        $username: String!
+        $email: String!
+        $password: String!
+        ) {
+        addUser(
+            username: $username
+            email: $email
+            password: $password
+            ) {
+                token
+                user {
+                    _id
+                    username
+                }
+            }
+        }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($newBook: InputBook!) {
+        saveBook(newBook: $newBook) {
             _id
             username
-        }
-        }
-    }
-    `;
-
-    export const ADD_USER = gql`
-    mutation addUser($username: String!, $email: String!, $password: String!) {
-        addUser(username: $username, email: $email, password: $password) {
-        token
-        user {
-            _id
-            username
-        }
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
         }
     }
-    `;
+`;
 
-    export const SAVE_BOOK = gql`
-    mutation saveBook($bookData: BookInput!) {
-        saveBook(bookData: $bookData) {
-        _id
-        username
-        email
-        savedBooks {
-            bookId
-            authors
-            image
-            description
-            title
-            link
-        }
-        }
-    }
-    `;
-
-    export const REMOVE_BOOK = gql`
+export const REMOVE_BOOK = gql`
     mutation removeBook($bookId: ID!) {
         removeBook(bookId: $bookId) {
-        _id
-        username
-        email
-        savedBooks {
-            bookId
-            authors
-            image
-            description
-            title
-            link
-        }
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
         }
     }
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,19 +1,20 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
-export const QUERY_ME = gql`
-    {
+
+export const GET_ME = gql`
+    query me {
         me {
-        _id
-        username
-        email
-        savedBooks {
-            bookId
-            authors
-            image
-            description
-            title
-            link
-        }
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
         }
     }
-`;
+    `;
